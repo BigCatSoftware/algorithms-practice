40f4ed582214ce3555204b448eaa31dc8257199d
Tiger <143446950+BigCatSoftware@users.noreply.github.com>
2025-12-02 23:33:19 -0800

    Add solution for maximum water container problem
    Implement maxArea method to calculate maximum water container area.

---
+++ container_with_most_water.py
@@ -0,0 +1,22 @@
+"""
+02DEC2025
+https://neetcode.io/problems/max-water-container/question
+"""
+
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        i, j = 0, len(heights) - 1
+        max_area = 0
+
+        while i < j:
+            width = j - i
+            height = min(heights[i], heights[j])
+            curr_area = width * height
+            max_area = max(max_area, curr_area)
+            if heights[i] <= heights[j]:
+                i += 1
+            else:
+                j -= 1
+
+        return max_area
+        
