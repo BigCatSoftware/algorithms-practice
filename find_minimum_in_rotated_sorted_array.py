f157f8af754626c7d36ebcd86fb4d238a0670d4f
Tiger <143446950+BigCatSoftware@users.noreply.github.com>
2025-12-05 00:28:21 -0800

    Add function to find minimum in rotated sorted array
    Implement binary search to find the minimum in a rotated sorted array.

---
+++ find_minimum_in_rotated_sorted_array.py
@@ -0,0 +1,24 @@
+"""
+04DEC2025
+https://neetcode.io/problems/find-minimum-in-rotated-sorted-array/question
+"""
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        res = nums[0]
+
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            if nums[left] < nums[right]:
+                res = min(res, nums[left])
+                break
+
+            mid = left + (right - left) // 2
+            res = min(res, nums[mid])
+            if nums[left] <= nums[mid]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return res
+        
