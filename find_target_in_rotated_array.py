cac7a0b7e7cc8e92bc19812721e3fa8fea2150ee
Tiger <143446950+BigCatSoftware@users.noreply.github.com>
2025-12-06 00:49:58 -0800

    Add search method to find target in rotated array
    Implement binary search for finding target in rotated sorted array.

---
+++ find_target_in_rotated_array.py
@@ -0,0 +1,36 @@
+"""
+05DEC2025
+https://neetcode.io/problems/find-target-in-rotated-sorted-array/question
+"""
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[right] < nums[mid]:
+                left = mid + 1
+            else:
+                right = mid
+
+        pivot = left
+        left, right = 0, len(nums) - 1
+
+        if target >= nums[pivot] and target <= nums[right]:
+            left = pivot
+        else:
+            right = pivot - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
+
+        
