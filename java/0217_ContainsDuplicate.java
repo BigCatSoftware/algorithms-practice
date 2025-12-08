58de98c3a44328fec2e2725b71dc5e6315f9c360
Tiger <143446950+BigCatSoftware@users.noreply.github.com>
2025-12-08 00:49:48 -0800

    Implement containsDuplicate method in Solution class

---
+++ java/0217_ContainsDuplicate.java
@@ -0,0 +1,19 @@
+/*
+08DEC2025
+https://leetcode.com/problems/contains-duplicate/
+*/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+
+        return false;
+    }
+}
